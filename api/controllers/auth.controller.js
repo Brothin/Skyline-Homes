d5ae2d03c69181514bc4815fdc29d4972077a38d
Brothin <brothindas007@gmail.com>
2023-11-12 11:46:12 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User created successfully");
+    res.status(201).json("User created successfully!");
   } catch (err) {
     next(err);
   }
@@ -20,11 +20,11 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(404, "User not found"));
+      return next(errorHandler(404, "User not found!"));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(401, "Wrong credentials"));
+      return next(errorHandler(401, "Wrong credentials!"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
